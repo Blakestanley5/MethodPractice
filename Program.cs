f5199453a92f7d9ee453431d8c050d59f0a38a22
BlakeStanley5 <blakestanley542@gmail.com>
2020-10-23 14:06:57 -0500

    practice

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Dynamic;
+
+namespace MethodsPractice2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("hi what is your name?");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("What is your favorite color?");
+            string color = Console.ReadLine();
+
+            Console.WriteLine("What is you favorite animal?");
+            string animal = Console.ReadLine();
+
+            Console.WriteLine("What is your favorite band?");
+            string band = Console.ReadLine();
+
+            Console.WriteLine($"Today is the day {name} has been waiting on all year! {name} woke up early to get ready and head to see his favorite band play {band}. He was in such a hurry to get to the concert he almost forgot to feed his pet {animal}! After feeding his {animal} he jumped in his {color} car and headed to the show. The {band} didnt disappoint and overall it was a great day!");
+
+            static int Add(int y, int x)
+            {
+                Console.WriteLine($"We are going to add {x} and {y}");
+                return y + x;
+
+            }
+
+            
+
+            int result = Add(2, 4);
+            Console.WriteLine(result);
+
+
+
+
+        }
+    }
+}
